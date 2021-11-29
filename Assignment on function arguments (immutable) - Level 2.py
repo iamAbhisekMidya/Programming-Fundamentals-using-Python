84d8be85911d144b065593ac9dc44a1c663560dd
Abhisek Midya <mid.abhi2000@gmail.com>
2021-11-29 23:27:09 +0530

    Create Assignment on function arguments (immutable) - Level 2.py

---
+++ Assignment on function arguments (immutable) - Level 2.py
@@ -0,0 +1,26 @@
+'''
+Write a python function, check_double(number) which accepts a whole number and returns True if it satisfies the given conditions.
+
+The number and its double should have exactly the same number of digits.
+
+Both the numbers should have the same digits ,but in different order.
+
+Otherwise it should return False.
+
+Example: If the number is 125874 and its double, 251748, contain exactly the same digits, but in a different order.
+'''
+
+def check_double(number):
+    n=number*2
+    lst1=[int(x) for x in str(number)]
+    lst2=[int(x) for x in str(n)]
+    if len(lst1)!=len(lst2):
+        return False
+    for x in lst1:
+        if x not in lst2:
+            return False
+    
+    return True
+    
+#Provide different values for number and test your program
+print(check_double(125874))
